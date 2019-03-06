995813b885d00127290d7e76d7342b46af1b24ff
visualstudio <visualstudio@122-PC40>
2019-03-06 10:41:39 -0500

    lab 16

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp6
+namespace DelegatesAndEvents
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+            Console.ReadLine();
+
         }
     }
+
+    public delegate void MyDelegate(ref int intValue);
+
+    public class DelegateExercises
+    {
+        void Method1(ref int intValue)
+        {
+            intValue = intValue + 5;
+            System.Console.WriteLine("Method1 " + intValue);
+        }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            MyDelegate myDelegate1 = new MyDelegate(Method1);
+            MyDelegate myDelegate2 = myDelegate + myDelegate1;
+            int intParameter = 5;
+            myDelegate2(ref intParameter);
+        }
+    }
+
 }
